02fa2a0e4b4abc8b6e4cafaf81e356755e99ee32
Xian Li <63030442+lx2009xian@users.noreply.github.com>
2021-05-19 21:12:19 +0800

    Update DIV*.py

--- DIV*.py
+++ DIV*.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # coding: utf-8
 
 
@@ -9,13 +8,11 @@ import pandas as pd
 
 
 
-
 # Calculate the variety
-data=pd.read_csv("C:/Users/Lenovo/Desktop/Demo/result.csv",header=0,index_col=0)
+data=pd.read_excel("C:/Users/86198/Desktop/three components/result.xlsx",header=0,index_col=0)
 data[data>1]=1 #In order to change the cell>1 to =1
 a=data[data>0].count()#count the number of item of given papers
 a=a.to_frame()
-
 
 
 
@@ -28,24 +25,25 @@ new_diversity=pd.DataFrame(np.zeros((data.shape[1],1)),index=data.columns.values
 
 
 #Input the balance
-balance=pd.read_excel("C:/Users/Lenovo/Desktop/Demo/balance.xlsx",header=0,index_col=0)#input balance
+balance=pd.read_excel("C:/Users/86198/Desktop/three components/balance.xlsx",header=0,index_col=0)#input balance
 
 
 
 
 #Input the disparity matrix（use the 1-cosine as the distance measurement）
-disparity_matrix=pd.read_excel("C:/Users/Lenovo/Desktop/Demo/1-cosine.xlsx",header=0,index_col=0)#input disparity matrix
-
+disparity_matrix=pd.read_excel("C:/Users/86198/Desktop/three components/1-cosine.xlsx",header=0,index_col=0)#input disparity matrix
 
 
 
 #Calculate the cumulative sum of dij 
 for i in range(0,data.shape[1]):
-    data_array=data.iloc[:,i].values
-    data_arrayt=np.transpose(data_array)
-    cumulative_sum=np.dot(np.dot(data_arrayt,disparity_matrix),data_array)
-    cumulative_sum_f.iloc[i,0]=cumulative_sum
-
+    if a.iloc[i,0]==1:
+         cumulative_sum_f.iloc[i,0]=0
+    else:
+        data_array=data.iloc[:,i].values
+        data_arrayt=np.transpose(data_array)
+        cumulative_sum=np.dot(np.dot(data_arrayt,disparity_matrix),data_array)
+        cumulative_sum_f.iloc[i,0]=cumulative_sum/(a.iloc[i,0]*(a.iloc[i,0]-1))
 
 
 
@@ -53,10 +51,5 @@ for i in range(0,data.shape[1]):
 for j in range(0,data.shape[1]):
     diversity=a.iloc[j,0]*balance.iloc[j,0]*cumulative_sum_f.iloc[j,0]
     new_diversity.iloc[j,0]=diversity
-new_diversity.to_csv("C:/Users/Lenovo/Desktop/Demo/new_diveristy.csv")
-
-
-
-
-
+new_diversity.to_excel("C:/Users/86198/Desktop/three components/new_diveristy.xlsx")
 
